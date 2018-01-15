1ea0a4fe81c872992491e202aaea9ead9672aec1
Renel972 <renel972@users.noreply.github.com>
2018-01-15 22:59:17 +0100

    c

--- home/main.bundle.js
+++ home/main.bundle.js
@@ -38,7 +38,8 @@ var AppMaterialModule = /** @class */ (function () {
     AppMaterialModule = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["H" /* NgModule */])({
             exports: [
-                __WEBPACK_IMPORTED_MODULE_1__angular_material__["a" /* MatToolbarModule */]
+                __WEBPACK_IMPORTED_MODULE_1__angular_material__["b" /* MatToolbarModule */],
+                __WEBPACK_IMPORTED_MODULE_1__angular_material__["a" /* MatSidenavModule */]
             ]
         })
     ], AppMaterialModule);
@@ -70,7 +71,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"app-container\" [class.app-is-mobile]>\n  <mat-toolbar color=\"primary\" class=\"app-toolbar\">\n    <img width=\"50\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n    <h1 style=\"margin-left: 8px;\"> {{ title }}! </h1>\n  </mat-toolbar>\n</div>\n"
+module.exports = "<div class=\"app-container\" [class.app-is-mobile]>\n  <mat-toolbar color=\"primary\" class=\"app-toolbar\">\n    <img width=\"50\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n    <h1 style=\"margin-left: 8px;\"> {{ title }}! </h1>\n  </mat-toolbar>\n\n  <mat-sidenav-container class=\"app-sidenav-container\" [style.marginTop.px]=\"56\">\n    <mat-sidenav #snav mode=\"over\">\n      mat-sidenav\n    </mat-sidenav>\n\n    <mat-sidenav-content>\n      mat-sidenav-content\n    </mat-sidenav-content>\n  </mat-sidenav-container>\n</div>\n"
 
 /***/ }),
 
